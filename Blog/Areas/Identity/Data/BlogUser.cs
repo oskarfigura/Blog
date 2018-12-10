88709886eb1d41620e2938cf096ec3323d1bdacb
oskarfigura <contact@oskarfigura.com>
2018-12-10 08:42:03 +0000

    Added custom user fields

--- Blog/Areas/Identity/Data/BlogUser.cs
+++ Blog/Areas/Identity/Data/BlogUser.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
 namespace Blog.Areas.Identity.Data
 {
-    // Add profile data for application users by adding properties to the BlogUser class
     public class BlogUser : IdentityUser
     {
+        [Required]
+        [PersonalData]
+        [StringLength(50, ErrorMessage = "Full name cannot be longer than 50 characters.")]
+        [Display(Name = "Full Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [PersonalData]
+        [DataType(DataType.Text)]
+        [StringLength(15, MinimumLength = 3,
+            ErrorMessage = "Display name must be a minimum of 3 characters and cannot be longer than 15 characters ")]
+        [Display(Name = "Display Name")]
+        public string DisplayName { get; set; }
+
+        [Required]
+        [PersonalData]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public override string Email { get; set; }
     }
 }
