004e4199d0c91fc7d30bb7ec5a036d47813d6c72
oskarfigura <contact@oskarfigura.com>
2018-12-11 22:37:04 +0000

    Added Index method

---
+++ Blog/Controllers/AccountManagerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Areas.Identity.Data;
+using Blog.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+
+namespace Blog.Controllers
+{
+    public class AccountManagerController : Controller
+    {
+
+        private readonly IUserRepo _userRepo;
+
+        public AccountManagerController(IUserRepo userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        public async Task<IActionResult> Index(AccountManagerViewModel accountManagerViewModel)
+        {
+            var accountSearchData = accountManagerViewModel.AccountSearch;
+
+            return View(new AccountManagerViewModel()
+            {
+                Accounts = await _userRepo.GetUsersBySearchData(accountManagerViewModel.AccountSearch),
+                AvailableIdentityRoles = await _userRepo.GetAllRoles(),
+                AccountSearch = accountSearchData
+            });
+        }
+    }
+}
