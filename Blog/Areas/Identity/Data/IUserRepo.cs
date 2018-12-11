d4622cc6ce3045039844d051dee4dcde8780d5eb
oskarfigura <contact@oskarfigura.com>
2018-12-11 22:35:30 +0000

    UserRepo working GetRoles and GetUsersBySearchData

--- Blog/Areas/Identity/Data/IUserRepo.cs
+++ Blog/Areas/Identity/Data/IUserRepo.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Blog.Areas.Identity.Data
 {
     public interface IUserRepo : IDisposable
     {
-        Task<IEnumerable<BlogUser>> GetUsers();
-        Task<BlogUser> GetUserById(string userId);
-        void InsertUser(BlogUser user);
-        void DeleteUser(string userId);
-        void UpdateUser(BlogUser user);
+        Task<IEnumerable<AccountViewModel>> GetUsersBySearchData(AccountSearch searchModel);
+        Task<IEnumerable<IdentityRole>> GetAllRoles();
+//        Task<BlogUser> GetUserById(string userId);
+//        void InsertUser(BlogUser user);
+//        void DeleteUser(string userId);
+//        void UpdateUser(BlogUser user);
         void Save();
     }
 }
--- Blog/Areas/Identity/Data/UserRepo.cs
+++ Blog/Areas/Identity/Data/UserRepo.cs
@@ -13,6 +13,7 @@ namespace Blog.Areas.Identity.Data
     {
         private readonly BlogContext _context;
         private readonly UserManager<BlogUser> _userManager;
+        private bool _disposed = false;
 
         public UserRepo(BlogContext context, UserManager<BlogUser> userManager)
         {
@@ -20,64 +21,126 @@ namespace Blog.Areas.Identity.Data
             _userManager = userManager;
         }
 
-        public async Task<IEnumerable<BlogUser>> GetUsers()
+        public async Task<IEnumerable<AccountViewModel>> GetUsersBySearchData(AccountSearch searchModel)
         {
-            return await _context.Users.ToListAsync();
+            var result = await GetUsers();
+
+            if (searchModel == null) return result;
+
+            if (!string.IsNullOrEmpty(searchModel.UserName))
+            {
+                result = result.Where(x => x.UserName.Contains(searchModel.UserName,
+                    StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (!string.IsNullOrEmpty(searchModel.Role))
+            {
+                result = result.Where(x => x.Role.Contains(searchModel.Role,
+                    StringComparison.OrdinalIgnoreCase));
+            }
+
+            return result;
         }
 
-        public async Task<BlogUser> GetUserById(string id)
+        public async Task<IEnumerable<IdentityRole>> GetAllRoles()
         {
-            return await _userManager.FindByIdAsync(id);
+            return await _context.Roles.ToListAsync();
         }
 
-        public async void InsertUser(BlogUser user)
-        {
-            await _context.Users.AddAsync(user);
-        }
-
-        public async void DeleteUser(string userId)
-        {
-            BlogUser user = await _userManager.FindByIdAsync(userId);
-            await _userManager.DeleteAsync(user);
-        }
-
-        public async void UpdateUser(BlogUser user)
-        {
-            await _userManager.UpdateAsync(user); 
-        }
-
-        /***
-         * 
-         * */
+//        public async Task<BlogUser> GetUserById(string id)
+//        {
+//            return await _userManager.FindByIdAsync(id);
+//        }
+//
+//        public async void InsertUser(BlogUser user)
+//        {
+//            await _context.Users.AddAsync(user);
+//        }
+//
+//        public async void DeleteUser(string userId)
+//        {
+//            var user = await _userManager.FindByIdAsync(userId);
+//            await _userManager.DeleteAsync(user);
+//        }
+//
+//        public async void UpdateUser(BlogUser user)
+//        {
+//            await _userManager.UpdateAsync(user); 
+//        }
 
         public async void Save()
         {
            await _context.SaveChangesAsync();
         }
 
-        private bool disposed = false;
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!this.disposed)
-            {
-                if (disposing)
-                {
-
-                    if (this._userManager != null)
-                    {
-                        this._userManager.Dispose();
-                    }
-                    _context.Dispose();
-                }
-            }
-            this.disposed = true;
-        }
-
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                    _userManager?.Dispose();
+                    _context.Dispose();
+                }
+            }
+            this._disposed = true;
+        }
+
+        private async Task<IEnumerable<BlogUser>> GetBlogUsersFromDb()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        private async Task<IEnumerable<AccountViewModel>> GetUsers()
+        {
+            var users = await GetBlogUsersFromDb();
+            var result = new List<AccountViewModel>();
+
+            foreach (var user in users)
+            {
+                var role = await GetUsersRoleName(user.Id);
+
+                result.Add(new AccountViewModel()
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    DisplayName = user.DisplayName,
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    EmailConfirmed = user.EmailConfirmed,
+                    PhoneNumber = user.PhoneNumber,
+                    PhoneNumberConfirmed = user.PhoneNumberConfirmed,
+                    Role = role
+                });
+            }
+
+            return result;
+        }
+
+        private async Task<string> GetUsersRoleName(string userId)
+        {
+            var usersRoleId = await GetUsersRoleId(userId);
+            var usersIdentityRole = await GetRoleById(usersRoleId);
+
+            return usersIdentityRole.Name;
+        }
+
+        private async Task<string> GetUsersRoleId(string userId)
+        {
+            var identityUserRole = await _context.UserRoles.Where(x => x.UserId.Equals(userId)).FirstAsync();
+            return identityUserRole.RoleId;
+        }
+
+        private async Task<IdentityRole> GetRoleById(string roleId)
+        {
+            var role = await _context.Roles.FindAsync(roleId);
+            return role;
         }
     }
 }
