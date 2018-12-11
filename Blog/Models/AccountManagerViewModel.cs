3a4163cb7ab96a230748e08e8c071529234f2800
oskarfigura <contact@oskarfigura.com>
2018-12-11 19:34:45 +0000

    Add account manager and account view model

---
+++ Blog/Models/AccountManagerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Models
+{
+    /**
+     * View model used for account manager
+     */
+    public class AccountManagerViewModel
+    {
+        public List<AccountViewModel> Accounts { get; set; }
+        public List<IdentityRole> AvailableIdentityRoles { get; set; }
+    }
+}
---
+++ Blog/Models/AccountViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Areas.Identity.Data;
+
+namespace Blog.Models
+{
+    /**
+     * Used for displaying account data including the role
+     */
+    public class AccountViewModel : BlogUser
+    {
+        public string Role { get; set; }
+    }
+}
