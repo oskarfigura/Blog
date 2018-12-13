7aff89a0151d7702e20c85041b0f0c4e235d5bb0
oskarfigura <contact@oskarfigura.com>
2018-12-13 22:44:45 +0000

    Created BlogController with post view

--- Blog/Controllers/BlogController.cs
+++ Blog/Controllers/BlogController.cs
@@ -2,14 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Areas.Identity.Data;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
     public class BlogController : Controller
     {
+
+        private readonly IPostRepo _postRepo;
+
+        public BlogController(IPostRepo postRepo)
+        {
+            _postRepo = postRepo;
+        }
+
         public IActionResult Index()
         {
+            //TODO HOME PAGE list all posts 
+            return View();
+        }
+
+        [Route("/Blog/{slug?}")]
+        public async Task<IActionResult> Post(string slug)
+        {
+            if (string.IsNullOrEmpty(slug))
+            {
+                return NotFound();
+            }
+
+            var post = await _postRepo.GetPostBySlug(slug);
+
+            if (string.IsNullOrEmpty(post.Id))
+            {
+                return NotFound();
+            }
+
+//            return View(new PostViewModel()
+//            {
+//                //TODO Set model
+//            });
             return View();
         }
     }
