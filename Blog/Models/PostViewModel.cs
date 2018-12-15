bdc5a62e3e04bf8b519bf3bcdd2db17e6b84f7ba
oskarfigura <contact@oskarfigura.com>
2018-12-15 14:38:46 +0000

    Added Post view model

--- Blog/Models/PostViewModel.cs
+++ Blog/Models/PostViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,32 @@ namespace Blog.Models
 {
     public class PostViewModel
     {
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Slug Url")]
+        public string Slug { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.Html)]
+        public string Content { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM-dd-yyyy hh:mm tt}")]
+        [Display(Name = "Date Posted")]
+        public DateTime PubDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM-dd-yyyy hh:mm tt}")]
+        [Display(Name = "Date Edited")]
+        public DateTime EditDate { get; set; }
+
+        public IEnumerable<Comment> Comments { get; set; } = new List<Comment>();
 
     }
 }
