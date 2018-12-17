3a9dcde058c4bd656d8d29d8a113aa765eecb550
oskarfigura <contact@oskarfigura.com>
2018-12-17 20:23:44 +0000

    Logout now redirects to home-page

--- Blog/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Blog/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -31,14 +31,8 @@ namespace Blog.Areas.Identity.Pages.Account
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                return Page();
-            }
+
+            return RedirectToAction("Index", "Blog");
         }
     }
 }
