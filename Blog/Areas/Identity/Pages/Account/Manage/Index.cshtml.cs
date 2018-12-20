bf250d7a4c9dc7df82b6ab464dd5d2106bbd90f1
oskarfigura <contact@oskarfigura.com>
2018-12-20 22:43:27 +0000

    Fixed updating user data

--- Blog/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Blog/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -134,6 +134,7 @@ namespace Blog.Areas.Identity.Pages.Account.Manage
                 }
             }
 
+            await _userManager.UpdateAsync(user);
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
             return RedirectToPage();
