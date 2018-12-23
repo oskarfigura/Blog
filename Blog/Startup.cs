74a0970a2c58143d6059e584d285a02884ba7463
oskarfigura <contact@oskarfigura.com>
2018-12-23 15:36:28 +0000

    Slight changes to cookies

--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -29,13 +29,6 @@ namespace Blog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
             services.AddDbContext<BlogContext>(options => options.UseSqlServer(
                 Configuration.GetConnectionString("BlogContextConnection")));
 
@@ -48,6 +41,13 @@ namespace Blog
             services.AddTransient<IPostRepo, PostRepo>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+                options.HttpOnly = HttpOnlyPolicy.Always;
+                options.Secure = CookieSecurePolicy.Always;
+            });
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
 
